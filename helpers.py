fa3dd01799342afaa0c9aadd3abc5f4774be1e5b
dni ⚡ <office@dnilabs.com>
2025-03-14 10:50:35 +0100

    feat: remove atm endpoints and helpers for lnpos (#11)
    after talk oto ben atm functionality should lie in the fossa extension

--- helpers.py
+++ helpers.py
@@ -1,53 +1,7 @@
-import base64
 import hmac
 from io import BytesIO
-from typing import Optional
 
 from embit import compact
-from lnbits.helpers import urlsafe_short_hash
-from lnbits.utils.exchange_rates import fiat_amount_as_satoshis
-
-from .crud import create_lnpos_payment, get_recent_lnpos_payment
-from .models import Lnpos, LnposPayment
-
-
-async def register_atm_payment(
-    device: Lnpos, payload: str
-) -> tuple[Optional[LnposPayment], Optional[int]]:
-    """
-    Register an ATM payment to avoid double pull.
-    """
-    # create a new lnurlpayment record
-    data = base64.urlsafe_b64decode(payload)
-    payload = payload.replace("=", "")
-    decrypted = xor_decrypt(device.key.encode(), data)
-
-    lnpos_payment = await get_recent_lnpos_payment(payload)
-    # If the payment is already registered and been paid, return None
-    if lnpos_payment and lnpos_payment.payload == lnpos_payment.payment_hash:
-        return None, lnpos_payment.sats * 1000
-    # If the payment is already registered and not been paid, return lnurlpayment record
-    if lnpos_payment and lnpos_payment.payload != lnpos_payment.payment_hash:
-        return lnpos_payment, lnpos_payment.sats * 1000
-
-    price_msat = (
-        await fiat_amount_as_satoshis(float(decrypted[1]) / 100, device.currency) * 1000
-        if device.currency != "sat"
-        else decrypted[1] * 1000
-    )
-    price_msat = int(price_msat - ((price_msat / 100) * device.profit))
-    sats = int(price_msat / 1000)
-    lnpos_payment = LnposPayment(
-        id=urlsafe_short_hash(),
-        lnpos_id=device.id,
-        payload=payload,
-        sats=sats,
-        pin=int(decrypted[0]),
-        payment_hash="payment_hash",
-    )
-    lnpos_payment = await create_lnpos_payment(lnpos_payment)
-    price_msat = sats * 1000
-    return lnpos_payment, price_msat
 
 
 def xor_decrypt(key, blob):
