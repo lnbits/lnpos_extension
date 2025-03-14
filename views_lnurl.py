464321adc86c2168545bb6d147d150bd1c6830e5
dni ⚡ <office@dnilabs.com>
2025-03-14 11:47:54 +0100

    feat: use LnurlErrorResponseHandler (#17)
    now does not return dicts, instead the response handler takes care

--- views_lnurl.py
+++ views_lnurl.py
@@ -1,9 +1,10 @@
 import base64
 from http import HTTPStatus
 
-from fastapi import APIRouter, Query, Request
+from fastapi import APIRouter, HTTPException, Query, Request
 from lnbits.core.services import create_invoice
 from lnbits.helpers import urlsafe_short_hash
+from lnbits.lnurl import LnurlErrorResponseHandler
 from lnbits.utils.exchange_rates import fiat_amount_as_satoshis
 
 from .crud import (
@@ -16,6 +17,7 @@ from .helpers import xor_decrypt
 from .models import LnposPayment
 
 lnpos_lnurl_router = APIRouter(prefix="/api/v1/lnurl")
+lnpos_lnurl_router.route_class = LnurlErrorResponseHandler
 
 
 @lnpos_lnurl_router.get(
@@ -30,10 +32,7 @@ async def lnurl_v1_params(
 ):
     lnpos = await get_lnpos(lnpos_id)
     if not lnpos:
-        return {
-            "status": "ERROR",
-            "reason": f"lnpos {lnpos_id} not found on this server",
-        }
+        raise HTTPException(HTTPStatus.NOT_FOUND, detail="lnpos not found.")
 
     if len(p) % 4 > 0:
         p += "=" * (4 - (len(p) % 4))
@@ -42,7 +41,9 @@ async def lnurl_v1_params(
     try:
         pin, amount_in_cent = xor_decrypt(lnpos.key.encode(), data)
     except Exception as exc:
-        return {"status": "ERROR", "reason": str(exc)}
+        raise HTTPException(
+            HTTPStatus.BAD_REQUEST, detail=f"decryption error: {exc!s}"
+        ) from exc
 
     price_msat = (
         await fiat_amount_as_satoshis(float(amount_in_cent) / 100, lnpos.currency)
@@ -50,7 +51,7 @@ async def lnurl_v1_params(
         else amount_in_cent
     )
     if price_msat is None:
-        return {"status": "ERROR", "reason": "Price fetch error."}
+        raise HTTPException(HTTPStatus.BAD_REQUEST, detail="Price fetch error.")
 
     price_msat = int(price_msat * ((lnpos.profit / 100) + 1))
 
@@ -81,10 +82,10 @@ async def lnurl_v1_params(
 async def lnurl_callback(request: Request, payment_id: str):
     lnpos_payment = await get_lnpos_payment(payment_id)
     if not lnpos_payment:
-        return {"status": "ERROR", "reason": "lnpos_payment not found."}
+        raise HTTPException(HTTPStatus.NOT_FOUND, detail="lnpos_payment not found.")
     lnpos = await get_lnpos(lnpos_payment.lnpos_id)
     if not lnpos:
-        return {"status": "ERROR", "reason": "lnpos not found."}
+        raise HTTPException(HTTPStatus.NOT_FOUND, detail="lnpos not found.")
 
     payment = await create_invoice(
         wallet_id=lnpos.wallet,
