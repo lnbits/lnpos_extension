6d062dcb8b0d6733b46eaa12f44c039c17a432ac
dni ⚡ <office@dnilabs.com>
2025-03-17 09:53:43 +0100

    fixup

--- helpers.py
+++ helpers.py
@@ -4,8 +4,8 @@ from Cryptodome.Cipher import AES
 
 
 def aes_decrypt(key: str, iv: str, ciphertext: str) -> str:
-    _iv = urlsafe_b64decode(iv + '=' * (4 - len(iv) % 4))
-    _ct = urlsafe_b64decode(ciphertext + '=' * (4 - len(ciphertext) % 4))
+    _iv = urlsafe_b64decode(iv + "=" * (4 - len(iv) % 4))
+    _ct = urlsafe_b64decode(ciphertext + "=" * (4 - len(ciphertext) % 4))
     cipher = AES.new(key.encode(), AES.MODE_CBC, _iv)
     pt = cipher.decrypt(_ct)
     return pt.split(b"\x00")[0].decode()
