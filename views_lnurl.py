5f773a7350a76a3098f0843613ce9e5ed95b3887
dni ⚡ <office@dnilabs.com>
2025-04-30 10:41:08 +0200

    feat: change encryption to aes-cbc (#5) (#24)
    change up the encryption to make it more secure and auditable

--- views_lnurl.py
+++ views_lnurl.py
@@ -1,11 +1,12 @@
-import base64
 from http import HTTPStatus
+from math import ceil
 
 from fastapi import APIRouter, HTTPException, Query, Request
 from lnbits.core.services import create_invoice
 from lnbits.helpers import urlsafe_short_hash
 from lnbits.lnurl import LnurlErrorResponseHandler
 from lnbits.utils.exchange_rates import fiat_amount_as_satoshis
+from loguru import logger
 
 from .crud import (
     create_lnpos_payment,
@@ -13,7 +14,7 @@ from .crud import (
     get_lnpos_payment,
     update_lnpos_payment,
 )
-from .helpers import xor_decrypt
+from .helpers import aes_decrypt
 from .models import LnposPayment
 
 lnpos_lnurl_router = APIRouter(prefix="/api/v1/lnurl")
@@ -24,27 +25,37 @@ lnpos_lnurl_router.route_class = LnurlErrorResponseHandler
 async def lnurl_params(
     request: Request,
     lnpos_id: str,
-    p: str = Query(None),
+    payload: str = Query(..., alias="p"),
+    iv: str = Query(...),
 ):
     lnpos = await get_lnpos(lnpos_id)
     if not lnpos:
-        raise HTTPException(HTTPStatus.NOT_FOUND, detail="lnpos not found.")
+        raise HTTPException(HTTPStatus.NOT_FOUND, "lnpos not found.")
 
-    if len(p) % 4 > 0:
-        p += "=" * (4 - (len(p) % 4))
-
-    data = base64.urlsafe_b64decode(p)
+    if len(payload) % 22 != 0:
+        raise HTTPException(HTTPStatus.BAD_REQUEST, "Invalid payload length.")
+    if len(iv) != 22:
+        raise HTTPException(HTTPStatus.BAD_REQUEST, "Invalid IV length.")
+    lnpos_payment = await get_lnpos_payment(iv)
+    if lnpos_payment and lnpos_payment.lnpos_id != lnpos_id:
+        raise HTTPException(HTTPStatus.BAD_REQUEST, "Not your payment.")
+    if lnpos_payment and lnpos_payment.payment_hash:
+        raise HTTPException(HTTPStatus.BAD_REQUEST, "Payment already claimed.")
+    if lnpos_payment:
+        raise HTTPException(HTTPStatus.BAD_REQUEST, "Payment already registered.")
     try:
-        pin, amount_in_cent = xor_decrypt(lnpos.key.encode(), data)
-    except Exception as exc:
-        raise HTTPException(
-            HTTPStatus.BAD_REQUEST, detail=f"decryption error: {exc!s}"
-        ) from exc
+        msg = aes_decrypt(lnpos.key, iv, payload)
+    except Exception as e:
+        logger.debug(f"Error decrypting payload: {e}")
+        logger.debug(f"Payload: {payload}")
+        raise HTTPException(HTTPStatus.BAD_REQUEST, "Invalid payload.") from e
+
+    pin, amount_in_cent = msg.split(":")
 
     price_sat = (
         await fiat_amount_as_satoshis(float(amount_in_cent) / 100, lnpos.currency)
         if lnpos.currency != "sat"
-        else amount_in_cent
+        else ceil(float(amount_in_cent))
     )
     if price_sat is None:
         raise HTTPException(HTTPStatus.BAD_REQUEST, detail="Price fetch error.")
