9f836c63d3018d7a34de2a486f4570fcfd9cfce0
dni ⚡ <office@dnilabs.com>
2025-04-30 10:35:26 +0200

    feat: change encryption to aes-cbc (#5)
    change up the encryption to make it more secure and auditable

--- helpers.py
+++ helpers.py
@@ -1,42 +1,11 @@
-import hmac
-from io import BytesIO
+from base64 import urlsafe_b64decode
 
-from embit import compact
+from Cryptodome.Cipher import AES
 
 
-def xor_decrypt(key, blob):
-    s = BytesIO(blob)
-    variant = s.read(1)[0]
-    if variant != 1:
-        raise RuntimeError("Not implemented")
-    # reading nonce
-    nonce_len = s.read(1)[0]
-    nonce = s.read(nonce_len)
-    if len(nonce) != nonce_len:
-        raise RuntimeError("Missing nonce bytes")
-    if nonce_len < 8:
-        raise RuntimeError("Nonce is too short")
-
-    # reading payload
-    payload_len = s.read(1)[0]
-    payload = s.read(payload_len)
-    if len(payload) > 32:
-        raise RuntimeError("Payload is too long for this encryption method")
-    if len(payload) != payload_len:
-        raise RuntimeError("Missing payload bytes")
-    hmacval = s.read()
-    expected = hmac.new(
-        key, b"Data:" + blob[: -len(hmacval)], digestmod="sha256"
-    ).digest()
-    if len(hmacval) < 8:
-        raise RuntimeError("HMAC is too short")
-    if hmacval != expected[: len(hmacval)]:
-        raise RuntimeError("HMAC is invalid")
-    secret = hmac.new(key, b"Round secret:" + nonce, digestmod="sha256").digest()
-    payload = bytearray(payload)
-    for i in range(len(payload)):
-        payload[i] = payload[i] ^ secret[i]
-    s = BytesIO(payload)
-    pin = compact.read_from(s)
-    amount_in_cent = compact.read_from(s)
-    return str(pin), amount_in_cent
+def aes_decrypt(key: str, iv: str, ciphertext: str) -> str:
+    _iv = urlsafe_b64decode(iv + "=" * (4 - len(iv) % 4))
+    _ct = urlsafe_b64decode(ciphertext + "=" * (4 - len(ciphertext) % 4))
+    cipher = AES.new(key.encode(), AES.MODE_CBC, _iv)
+    pt = cipher.decrypt(_ct)
+    return pt.split(b"\x00")[0].decode()
