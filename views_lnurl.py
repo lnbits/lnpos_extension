906225293238bf0887914ba057205b5dde1b3de6
dni ⚡ <office@dnilabs.com>
2025-07-21 11:48:26 +0200

    refactor: use lnurl lib (#32)
    * refactor: use lnurl lib

--- views_lnurl.py
+++ views_lnurl.py
@@ -1,13 +1,24 @@
 from http import HTTPStatus
 from math import ceil
 
-from fastapi import APIRouter, HTTPException, Query, Request
+from fastapi import APIRouter, Query, Request
 from lnbits.core.services import create_invoice
 from lnbits.helpers import urlsafe_short_hash
-from lnbits.lnurl import LnurlErrorResponseHandler
 from lnbits.utils.crypto import AESCipher
 from lnbits.utils.exchange_rates import fiat_amount_as_satoshis
+from lnurl import (
+    CallbackUrl,
+    LightningInvoice,
+    LnurlErrorResponse,
+    LnurlPayActionResponse,
+    LnurlPayResponse,
+    LnurlPaySuccessActionTag,
+    Max144Str,
+    MilliSatoshi,
+    UrlAction,
+)
 from loguru import logger
+from pydantic import parse_obj_as
 
 from .crud import (
     create_lnpos_payment,
@@ -18,7 +29,6 @@ from .crud import (
 from .models import LnposPayment
 
 lnpos_lnurl_router = APIRouter(prefix="/api/v1/lnurl")
-lnpos_lnurl_router.route_class = LnurlErrorResponseHandler
 
 
 @lnpos_lnurl_router.get("/{lnpos_id}")
@@ -26,20 +36,20 @@ async def lnurl_params(
     request: Request,
     lnpos_id: str,
     payload: str = Query(..., alias="p"),
-):
+) -> LnurlPayResponse | LnurlErrorResponse:
     lnpos = await get_lnpos(lnpos_id)
     if not lnpos:
-        raise HTTPException(HTTPStatus.NOT_FOUND, "lnpos not found.")
+        return LnurlErrorResponse(reason="lnpos not found.")
 
     if len(payload) % 22 != 0:
-        raise HTTPException(HTTPStatus.BAD_REQUEST, "Invalid payload length.")
+        return LnurlErrorResponse(reason="Invalid payload length.")
     try:
         aes = AESCipher(lnpos.key)
         msg = aes.decrypt(payload, urlsafe=True)
     except Exception as e:
         logger.debug(f"Error decrypting payload: {e}")
         logger.debug(f"Payload: {payload}")
-        raise HTTPException(HTTPStatus.BAD_REQUEST, "Invalid payload.") from e
+        return LnurlErrorResponse(reason="Invalid payload.")
 
     pin, amount_in_cent = msg.split(":")
 
@@ -49,7 +59,7 @@ async def lnurl_params(
         else ceil(float(amount_in_cent))
     )
     if price_sat is None:
-        raise HTTPException(HTTPStatus.BAD_REQUEST, detail="Price fetch error.")
+        return LnurlErrorResponse(reason="Price fetch error.")
 
     price_sat = int(price_sat * ((lnpos.profit / 100) + 1))
     price_msat = price_sat * 1000
@@ -61,15 +71,20 @@ async def lnurl_params(
         pin=int(pin),
     )
     await create_lnpos_payment(lnpos_payment)
-    return {
-        "tag": "payRequest",
-        "callback": str(
-            request.url_for("lnpos.lnurl_callback", payment_id=lnpos_payment.id)
-        ),
-        "minSendable": price_msat,
-        "maxSendable": price_msat,
-        "metadata": lnpos.lnurlpay_metadata,
-    }
+
+    url = request.url_for("lnpos.lnurl_callback", payment_id=lnpos_payment.id)
+    callback_url = parse_obj_as(CallbackUrl, str(url))
+    return LnurlPayResponse(
+        callback=callback_url,
+        minSendable=MilliSatoshi(price_msat),
+        maxSendable=MilliSatoshi(price_msat),
+        metadata=lnpos.lnurlpay_metadata,
+        # TODO remove those when lib is updated
+        commentAllowed=None,
+        payerData=None,
+        allowsNostr=None,
+        nostrPubkey=None,
+    )
 
 
 @lnpos_lnurl_router.get(
@@ -77,13 +92,15 @@ async def lnurl_params(
     status_code=HTTPStatus.OK,
     name="lnpos.lnurl_callback",
 )
-async def lnurl_callback(request: Request, payment_id: str):
+async def lnurl_callback(
+    request: Request, payment_id: str
+) -> LnurlPayActionResponse | LnurlErrorResponse:
     lnpos_payment = await get_lnpos_payment(payment_id)
     if not lnpos_payment:
-        raise HTTPException(HTTPStatus.NOT_FOUND, detail="lnpos_payment not found.")
+        return LnurlErrorResponse(reason="lnpos_payment not found.")
     lnpos = await get_lnpos(lnpos_payment.lnpos_id)
     if not lnpos:
-        raise HTTPException(HTTPStatus.NOT_FOUND, detail="lnpos not found.")
+        return LnurlErrorResponse(reason="lnpos not found.")
 
     payment = await create_invoice(
         wallet_id=lnpos.wallet,
@@ -94,12 +111,15 @@ async def lnurl_callback(request: Request, payment_id: str):
     )
     lnpos_payment.payment_hash = payment.payment_hash
     lnpos_payment = await update_lnpos_payment(lnpos_payment)
-    return {
-        "pr": payment.bolt11,
-        "successAction": {
-            "tag": "url",
-            "description": "Check the attached link for the pin.",
-            "url": str(request.url_for("lnpos.displaypin", payment_id=payment_id)),
-        },
-        "routes": [],
-    }
+
+    pr = parse_obj_as(LightningInvoice, payment.bolt11)
+    callback_url = parse_obj_as(
+        CallbackUrl, str(request.url_for("lnpos.lnurl_callback", payment_id=payment_id))
+    )
+    action = UrlAction(
+        # TODO remove this when lib is updated
+        tag=LnurlPaySuccessActionTag.url,
+        description=Max144Str("Check the attached link for the pin."),
+        url=callback_url,
+    )
+    return LnurlPayActionResponse(pr=pr, successAction=action)
